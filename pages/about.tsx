9c869c393c408790433e9fa91d4a61b53fdb0678
Osaukhumwen Iyamuosa <osaukhu.bi@gmail.com>
2021-07-24 20:22:08 +0100

    make heritage section responsive

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,9 +1,11 @@
 import Head from "next/head";
+import Image from "next/image";
 
 import JakeImg from "../assets/images/leaders/avatar-jake.jpg";
 import ThompsonImg from "../assets/images/leaders/avatar-thompson.jpg";
 import JacksonImg from "../assets/images/leaders/avatar-jackson.jpg";
 import MariaImg from "../assets/images/leaders/avatar-maria.jpg";
+import HeritageImg from "../assets/images/image-heritage.jpg";
 
 import Tag from "../components/Tag";
 import Header from "../components/Header";
@@ -23,25 +25,37 @@ function About() {
       <Header />
       <PageHero />
       <section className={style.our_heritage}>
-        <h2 className={style.about_heading}>
-          <span>Our</span>
-          <span>Heritage</span>
-        </h2>
-        <p className={style.our_heritage_summary}>
-          Founded in 2007, we started as a trio of architects. Our complimentary
-          skills and relentless attention to detail turned Arch into one of the
-          most sought after boutique firms in the country.
-        </p>
-        <p className={style.our_heritage_summary}>
-          Speciliazing in Urban Design allowed us to focus on creating
-          exceptional structures that live in harmony with their surroundings.
-          We consider every detail from every surrounding element to inform our
-          designs.
-        </p>
-        <p className={style.our_heritage_summary}>
-          Our small team of world-class professionals provides input on every
-          project.
-        </p>
+        <div className={style.our_heritage_content}>
+          <div className={style.divider} />
+          <h2 className={style.about_heading}>
+            <span>Our</span>
+            <span>Heritage</span>
+          </h2>
+          <p className={style.our_heritage_summary}>
+            Founded in 2007, we started as a trio of architects. Our
+            complimentary skills and relentless attention to detail turned Arch
+            into one of the most sought after boutique firms in the country.
+          </p>
+          <p className={style.our_heritage_summary}>
+            Speciliazing in Urban Design allowed us to focus on creating
+            exceptional structures that live in harmony with their surroundings.
+            We consider every detail from every surrounding element to inform
+            our designs.
+          </p>
+          <p className={style.our_heritage_summary}>
+            Our small team of world-class professionals provides input on every
+            project.
+          </p>
+        </div>
+
+        <div className={style.our_heritage_image}>
+          <Image
+            src={HeritageImg}
+            alt="Heritage image"
+            layout="responsive"
+            objectFit="cover"
+          />
+        </div>
       </section>
       <section className={style.our_leaders}>
         <h2 className={style.about_heading}>
